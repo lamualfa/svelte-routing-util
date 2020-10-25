7146edefec88e3d849a210478580c20eb1ff5005
La Ode Muhammad Al Fatih <lamualfa@gmail.com>
2020-10-26 00:55:52 +0700

    Initial commit

---
+++ svelte-routing.config.js
@@ -0,0 +1,11 @@
+export default {
+  ssrBuildDir: 'build/ssr',
+  csrBuildDir: 'build/dist',
+  srcDir: 'src',
+  scriptFileName: 'main.js',
+  svelteFileName: 'App.svelte',
+  css: {
+    useCss: false,
+    buildFileName: 'bundle.css',
+  },
+};
